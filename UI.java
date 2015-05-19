229dfd910375bbfebcb2fe159ce4c49ae3114a62
xbrionx <xbrionx@outlook.com>
2015-05-19 13:34:00 -0700

    Update UI.java

--- UI.java
+++ UI.java
@@ -33,23 +33,36 @@ public class UI {
 	 * This method will start the game
 	 */
 	public void start() {
+		System.out.println("1. Start Game \n2. Load Game \n3. Debug Mode");
+		String x = ge.start(sc.nextLine());
+		if(x != "") {
+			options(x);
+		} else {
+			start();
+		}
+	}
+	
+	public void play() {
 		System.out.println(ge.getBoard());
-		System.out.println("1. Move \n2. Shoot \n3. Load \n4. Save \n5. Debug Mode");
-		options(ge.action(sc.nextInt()));
-		start();
+		System.out.println("1. Move \n2. Shoot \n3. Look \n4. Save");
+		options(ge.action(sc.nextLine()));
+		play();		
 	}
 	
 	/*
 	 * This method will print out the options to user.
 	 */
 	private void options(String s) {
+		if(s == "start") {
+			play();
+		}
 		if(s == "move") {
 			System.out.println("1. Move Up 2. Move Left 3. Move Right 4. Move Down");
-			ge.move(sc.nextInt());
+			ge.move(sc.nextLine());
 		}
 		if(s == "shoot") {
 			System.out.println("1. Shoot Up 2. Shoot Left 3. Shoot Right 4. Shoot Down");
-			ge.shoot(sc.nextInt());
+			ge.shoot(sc.nextLine());
 		}
 	}
 
